9f41b2990ea987a8c09d30c61dd2899b8812a5c8
Alex Garrett-Smith <alex@codecourse.com>
2024-07-08 09:43:24 +0100

    Episode 04

---
+++ resources/js/Components/Chat/ChatTextarea.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref} from "vue";
+
+const emit = defineEmits()
+
+const shift = ref(false)
+const model = ref('')
+
+const handleEnter = () => {
+    if (shift.value && model.value.length) {
+        model.value = model.value + '\n'
+        return
+    }
+
+    if (model.value.length) {
+        emit('valid', model.value)
+        model.value = ''
+    }
+}
+</script>
+
+<template>
+    <textarea
+        v-model="model"
+        rows="4"
+        id="body"
+        class="border-gray-200 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        v-on:keydown.enter.prevent="handleEnter"
+        v-on:keydown.shift="shift = true"
+        v-on:keyup="shift = false"
+    />
+</template>
--- resources/js/Pages/Room.vue
+++ resources/js/Pages/Room.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import ChatTextarea from "@/Components/Chat/ChatTextarea.vue";
 
 defineProps({
     room: Object
@@ -27,7 +28,11 @@ defineProps({
 
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-9">
                     <div class="p-6 text-gray-900">
-                        Messages
+                        <ChatTextarea
+                            class="w-full"
+                            placeholder="Say something..."
+                            v-on:valid="console.log($event)"
+                        />
                     </div>
                 </div>
             </div>
